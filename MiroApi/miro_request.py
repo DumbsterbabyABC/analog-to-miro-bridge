5456a3ec47de572b5ad9eee7ac91aeb38fb071bd
FrankSonnenberg <142150853+FrankSonnenberg@users.noreply.github.com>
2026-03-16 15:44:30 +0100

    Changed to Shape

--- MiroApi/miro_request.py
+++ MiroApi/miro_request.py
@@ -7,26 +7,38 @@ load_dotenv()
 token = os.getenv("MIRO_API_TOKEN")
 board_id = os.getenv("MIRO_BOARD_ID")
 
-url = f"https://api.miro.com/v2/boards/{board_id}/sticky_notes"
+import requests
 
+url = f"https://api.miro.com/v2/boards/{board_id}/shapes"
+
+payload = {
+    "data": {
+        "shape": "round_rectangle",
+        "content": "Test"
+    },
+    "style": {
+        "borderOpacity": "0",
+        "fillColor": "#ffffff",
+        "fontFamily": "arial",
+        "fontSize": "14",
+        "textAlign": "center",
+        "textAlignVertical": "middle"
+    },
+    "position": {
+        "x": 100,
+        "y": 100
+    },
+    "geometry": {
+        "height": 60,
+        "width": 320
+    }
+}
 headers = {
     "accept": "application/json",
     "content-type": "application/json",
     "authorization": f"Bearer {token}"
 }
 
-payload = {
-    "data": {
-        "content": "FIck Benke",
-        "shape": "square"
-    },
-    "position": {
-        "x": 0,
-        "y": 0,
-        "origin": "center"
-    }
-}
-
-response = requests.post(url, headers=headers, json=payload)
+response = requests.post(url, json=payload, headers=headers)
 
 print(response.text)
